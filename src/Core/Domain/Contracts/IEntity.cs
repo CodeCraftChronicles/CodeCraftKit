f3b769f5011a6154fc0a4d1e1af600d251711d2f
Daniel WOAGOU <pwoagou@otr.tg>
2024-07-17 13:32:34 +0000

    defining stucture

---
+++ src/Core/Domain/Contracts/IEntity.cs
@@ -0,0 +1,27 @@
+﻿namespace Core.Domain.Contracts;
+
+public interface IEntity
+{
+}
+public interface IEntity<TId> : IEntity
+{
+    public TId Id { get; set; }
+}
+
+public interface IAuditableEntity : IEntity
+{
+    string CreatedBy { get; set; }
+
+    DateTime CreatedOn { get; set; }
+
+    string LastModifiedBy { get; set; }
+
+    DateTime? LastModifiedOn { get; set; }
+
+    string DeletedBy { get; set; }
+    DateTime DeletedOn { get; set; }
+}
+public interface IAuditableEntity<TId> : IAuditableEntity, IEntity<TId>
+{
+}
+
---
+++ src/Shared/Exceptions/ApiException.cs
@@ -0,0 +1,22 @@
+﻿using Shared.Wrappers;
+
+using System.Globalization;
+
+namespace Shared.Exceptions;
+
+public class ApiException : Exception
+{
+    public ApiResponse.Error Error { get; set; }
+    public ApiException() : base()
+    {
+    }
+
+    public ApiException(string message) : base(message)
+    {
+    }
+
+    public ApiException(string message, params object[] args)
+        : base(string.Format(CultureInfo.CurrentCulture, message, args))
+    {
+    }
+}
